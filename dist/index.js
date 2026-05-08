b4975daa5ae1bb8c372cfc6c682ab15f2ad7a5a2
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-05-08 19:28:52 +0600

    learning interface

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const myName = "name";
-console.log(myName);
+export {};
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-const myName: string = "name"
-console.log(myName);
