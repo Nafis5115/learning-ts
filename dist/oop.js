931601ce9604e4bb937d729cfb668e5ed6bd058b
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-05-17 21:35:38 +0600

    completed learning type guard

--- dist/oop.js
+++ dist/oop.js
@@ -15,3 +15,29 @@ export {};
 // }
 // const student = new Student('shan', 22, 10)
 // const teacher = new Teacher('teacher', 40)
+// // Type guard - typeOf 
+// const add = (num1:number | string, num2:number| string) => {
+//     if(typeof num1 === 'number' && typeof num2 === 'number'){
+//     return num1 + num2
+//     }else{
+//        return num1.toString() + num2.toString()
+//     }
+// }
+// console.log(add(2, '4'));
+// // Type guard - in 
+// type NormalUser = {
+//     name:string,
+// }
+// type AdminUser = {
+//     name:string,
+//     role:'Admin'
+// }
+// const getUserRole = (user:NormalUser | AdminUser) => {
+// if('role' in user){
+//     console.log(user.role);
+// }
+// else{
+//     console.log("role not here");
+// }
+// }
+// getUserRole({name:'shan'})
